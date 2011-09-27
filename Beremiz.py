282362cef35ec8c87c237080fb634492d094d697
Edouard Tisserant <none@none>
2011-09-27 18:27:14 +0200

    Fixed wrong bug report email

--- Beremiz.py
+++ Beremiz.py
@@ -1542,7 +1542,7 @@ An unhandled exception (bug) occured. Bug report saved at :
 (%s)
 
 Please be kind enough to send this file to:
-dev@pibit.ch
+dev@automforge.net
 
 You should now restart Beremiz.
 
