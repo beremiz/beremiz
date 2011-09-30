eb434088c5bef91c8d2dce67164031daa649586e
Edouard Tisserant <none@none>
2011-09-30 23:41:46 +0200

    Default gcc toolchain config now use 'gcc' for linking. Was left accidentally to g++ since old times of wxSVG based SVGUI. Cleaned up gcc parameters when linking

--- targets/Win32/__init__.py
+++ targets/Win32/__init__.py
@@ -3,9 +3,4 @@ from .. import toolchain_gcc
 class Win32_target(toolchain_gcc):
     extension = ".dll"
     def getBuilderLDFLAGS(self):
-        return toolchain_gcc.getBuilderLDFLAGS(self) + ["-shared",
-                     "-Wl,--export-all-symbols",
-                     "-Wl,--enable-auto-import",
-                     "-Wl,--whole-archive",
-                     "-Wl,--no-whole-archive",
-                     "-Wl,--exclude-libs,All"]
+        return toolchain_gcc.getBuilderLDFLAGS(self) + ["-shared"]
