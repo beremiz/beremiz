d31b389ccfd1b982dc80ee17d2affb4066ebee38
Edouard Tisserant <none@none>
2013-07-26 14:04:29 +0900

    Added 'dlopen_prefix' class attributes to targets, in order to handle dlopen of shared object passed to runtime as extra file, and then in PWD (was working with windows only)

--- targets/Win32/__init__.py
+++ targets/Win32/__init__.py
@@ -1,6 +1,7 @@
 from ..toolchain_gcc import toolchain_gcc
 
 class Win32_target(toolchain_gcc):
+    dlopen_prefix = ""
     extension = ".dll"
     def getBuilderLDFLAGS(self):
         return toolchain_gcc.getBuilderLDFLAGS(self) + ["-shared", "-lwinmm"]
