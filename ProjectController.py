b4c6440ac1aa511369aadd5d1e958bd132fbc1b3
Edouard Tisserant <none@none>
2014-03-27 10:06:30 +0100

    Pair with matiec a51a3bb4d613. Re-enable resource add/remove, updated CFLAGS, disabled broken global FG test in tests/python

--- ProjectController.py
+++ ProjectController.py
@@ -440,7 +440,7 @@ class ProjectController(ConfigTreeNode, PLCControler):
         if len(self.Libraries)==0:
             return [],[],()
         self.GetIECProgramsAndVariables()
-        LibIECCflags = '"-I%s"'%os.path.abspath(self.GetIECLibPath())
+        LibIECCflags = '"-I%s" -Wno-unused-function'%os.path.abspath(self.GetIECLibPath())
         LocatedCCodeAndFlags=[]
         Extras=[]
         for lib in self.Libraries:
@@ -668,7 +668,7 @@ class ProjectController(ConfigTreeNode, PLCControler):
         # Keep track of generated C files for later use by self.CTNGenerate_C
         self.PLCGeneratedCFiles = C_files
         # compute CFLAGS for plc
-        self.plcCFLAGS = '"-I%s"'%self.ieclib_path
+        self.plcCFLAGS = '"-I%s" -Wno-unused-function'%self.ieclib_path
         return True
 
     def GetBuilder(self):
