170a5a5126a9c8ebc180e741869ae1dfb58948d1
Andrey Skvortsov <andrej.skvortzov@gmail.com>
2016-12-07 19:25:58 +0300

    always load existing PLC program from the working directory

--- Beremiz_service.py
+++ Beremiz_service.py
@@ -441,9 +441,9 @@ class Server():
             self.servicepublisher = ServicePublisher.ServicePublisher()
             self.servicepublisher.RegisterService(self.servicename, self.ip_addr, self.port)
 
-        if self.autostart :
-            self.plcobj.AutoLoad()
-            if self.plcobj.GetPLCstatus()[0] != "Empty":
+        self.plcobj.AutoLoad()
+        if self.plcobj.GetPLCstatus()[0] != "Empty":
+            if self.autostart :
                 self.plcobj.StartPLC()
 
         sys.stdout.flush()
