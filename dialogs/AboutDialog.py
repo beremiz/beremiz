c72ae844ab69ab1a8eeb5953f6e03fc07e6ae592
Andrey Skvortsov <andrej.skvortzov@gmail.com>
2017-01-13 15:03:35 +0300

    fix file header in AboutDialog.py

--- dialogs/AboutDialog.py
+++ dialogs/AboutDialog.py
@@ -1,21 +1,29 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# This file is part of Beremiz, a Integrated Development Environment for
+# programming IEC 61131-3 automates supporting plcopen standard and CanFestival.
+# This file is based on code written for Whyteboard project.
+#
 # Copyright (c) 2009, 2010 by Steven Sproat
+# Copyright (c) 2016 by Andrey Skvortsov <andrej.skvortzov@gmail.com>
 #
-# GNU General Public Licence (GPL)
+# See COPYING file for copyrights details.
 #
-# Whyteboard is free software; you can redistribute it and/or modify it under
-# the terms of the GNU General Public License as published by the Free Software
-# Foundation; either version 3 of the License, or (at your option) any later
-# version.
-# Whyteboard is distributed in the hope that it will be useful, but WITHOUT
-# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
-# details.
-# You should have received a copy of the GNU General Public License along with
-# Whyteboard; if not, write to the Free Software Foundation, Inc., 59 Temple
-# Place, Suite 330, Boston, MA  02111-1307  USA
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
 
 
 """
