52557c1c560868102f43a7f991485a3495148406
wuyangtang <wuyangtang@live.com>
2017-07-05 13:11:45 +0300

    fix target Xenomai missing functions

--- targets/Xenomai/plc_Xenomai_main.c
+++ targets/Xenomai/plc_Xenomai_main.c
@@ -363,3 +363,31 @@ void UnLockPython(void)
     }    /* as plc does not wait for lock. */
 }
 
+int CheckRetainBuffer(void)
+{
+	return 1;
+}
+
+void ValidateRetainBuffer(void)
+{
+}
+
+void InValidateRetainBuffer(void)
+{
+}
+
+void Retain(unsigned int offset, unsigned int count, void *p)
+{
+}
+
+void Remind(unsigned int offset, unsigned int count, void *p)
+{
+}
+
+void CleanupRetain(void)
+{
+}
+
+void InitRetain(void)
+{
+}
