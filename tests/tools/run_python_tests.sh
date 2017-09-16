0295155ef0e3aeb99f0da09309dc0f3bb3385fa3
Andrey Skvortsov <andrej.skvortzov@gmail.com>
2017-09-16 21:26:17 +0300

    add shell script to run all python tests and automatically handle Xvfb

---
+++ tests/tools/run_python_tests.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+
+export DISPLAY=:42
+Xvfb $DISPLAY -screen 0 1280x1024x24 &
+sleep 1
+
+ret=0
+DELAY=400
+KILL_DELAY=$(($DELAY + 30))
+timeout -k $KILL_DELAY $DELAY pytest --timeout=10 ./tests/tools
+ret=$?
+
+pkill -9 Xvfb
+
+exit $ret
