e3e0c6e757ad9936c128e3082ffd3119456b1616
Andrey Skvortsov <andrej.skvortzov@gmail.com>
2017-10-12 16:18:05 +0300

    fix pylint error '(too-many-function-args) Too many positional arguments for function call'

--- svgui/pyjs/lib/pyjslib.py
+++ svgui/pyjs/lib/pyjslib.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# pylint: disable=too-many-function-args
 
 # iteration from Bob Ippolito's Iteration in JavaScript
 
