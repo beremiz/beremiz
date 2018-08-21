72a70951847b788aae11183366f6e7237444d386
Edouard Tisserant <none@none>
2018-08-21 16:11:02 +0200

    Refactoring. Separated PLC Object, PYRO Server and MainWorker :  - PLC Object is now a Singleton, instantiated through runtime.CreatePLCObjectSingleton(...)  - Pyro server doesn't hold any reference to PLCObject, and doesn't create it anymore  - PLC Object class doesn't inherit from Pyro.ObjBase anymore  - Pyro related code moved to runtime.PyroServer.py  - MainWorker class moved to runtime/Worker.py  - Both PLC Object and MainWorker creation happens in runtime/__init__.py

---
+++ runtime/PyroServer.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# This file is part of Beremiz runtime.
+
+# Copyright (C) 2007: Edouard TISSERANT and Laurent BESSARD
+# Copyright (C) 2017: Andrey Skvortsov
+# Copyright (C) 2018: Edouard TISSERANT
+
+# See COPYING file for copyrights details.
+
+import sys
+
+import Pyro
+import Pyro.core as pyro
+import runtime
+from runtime.ServicePublisher import ServicePublisher
+
+class Server(object):
+    def __init__(self, servicename, ip_addr, port):
+        self.continueloop = True
+        self.daemon = None
+        self.servicename = servicename
+        self.ip_addr = ip_addr
+        self.port = port
+        self.servicepublisher = None
+
+    def _to_be_published(self):
+        return self.servicename is not None and \
+               self.ip_addr is not None and \
+               self.ip_addr != "localhost" and \
+               self.ip_addr != "127.0.0.1"
+
+    def PrintServerInfo(self):
+        print(_("Pyro port :"), self.port)
+
+        if self._to_be_published():
+            print(_("Publishing service on local network"))
+
+        sys.stdout.flush()
+
+    def PyroLoop(self, when_ready):
+        while self.continueloop:
+            Pyro.config.PYRO_MULTITHREADED = 0
+            pyro.initServer()
+            self.daemon = pyro.Daemon(host=self.ip_addr, port=self.port)
+
+            # pyro never frees memory after connection close if no timeout set
+            # taking too small timeout value may cause
+            # unwanted diconnection when IDE is kept busy for long periods
+            self.daemon.setTimeout(60)
+	
+            pyro_obj=Pyro.core.ObjBase()
+            pyro_obj.delegateTo(runtime.GetPLCObjectSingleton())
+
+            self.daemon.connect(pyro_obj, "PLCObject")
+
+            if self._to_be_published():
+                self.servicepublisher = ServicePublisher()
+                self.servicepublisher.RegisterService(self.servicename, self.ip_addr, self.port)
+
+            when_ready()
+            self.daemon.requestLoop()
+            self.daemon.sock.close()
+
+    def Restart(self):
+        self._stop()
+
+    def Quit(self):
+        self.continueloop = False
+        self._stop()
+
+    def _stop(self):
+        if self.servicepublisher is not None:
+            self.servicepublisher.UnRegisterService()
+            self.servicepublisher = None
+        self.daemon.shutdown(True)
+
+
+
--- runtime/__init__.py
+++ runtime/__init__.py
@@ -1,28 +1,32 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# This file is part of Beremiz runtime.
-#
-# Copyright (C) 2007: Edouard TISSERANT and Laurent BESSARD
-#
-# See COPYING.Runtime file for copyrights details.
-#
-# This library is free software; you can redistribute it and/or
-# modify it under the terms of the GNU Lesser General Public
-# License as published by the Free Software Foundation; either
-# version 2.1 of the License, or (at your option) any later version.
-
-# This library is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# Lesser General Public License for more details.
-
-# You should have received a copy of the GNU Lesser General Public
-# License along with this library; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
-
 from __future__ import absolute_import
-import os
+import traceback
 
-from runtime.PLCObject import PLCObject, PLCprint, MainWorker
-import runtime.ServicePublisher
+from runtime.Worker import worker
+MainWorker = worker()
+
+from runtime.PLCObject import PLCObject
+
+_PLCObjectSingleton = None
+
+def GetPLCObjectSingleton():
+    global _PLCObjectSingleton
+    assert(_PLCObjectSingleton is not None)
+    return _PLCObjectSingleton
+
+
+def LogMessageAndException(msg, exp=None):
+    global _PLCObjectSingleton
+    if exp is None:
+        exp = sys.exc_info()
+    if _PLCObjectSingleton is not None:
+        _PLCObjectSingleton.LogMessage(0, msg + '\n'.join(traceback.format_exception(*exp)))
+    else:
+        print(msg)
+        traceback.print_exception(*exp)
+
+def CreatePLCObjectSingleton(*args):
+    global _PLCObjectSingleton
+    _PLCObjectSingleton = PLCObject(*args)
