dac0f692883a242659f4a1a45dec4355065f4762
Andrey Skvortsov <andrej.skvortzov@gmail.com>
2018-10-05 13:11:28 +0300

    cleanup pyc files between running pytest
    this is need to avoid problem sometimes tests are run inside docker
    container and sometimes local.
    
    Traceback (most recent call last):
      File "/usr/lib/python2.7/dist-packages/_pytest/config/__init__.py", line 409, in _importconftest
        mod = conftestpath.pyimport()
      File "/usr/lib/python2.7/dist-packages/py/_path/local.py", line 686, in pyimport
        raise self.ImportMismatchError(modname, modfile, self)
    ImportMismatchError: ('conftest', '/beremiz/tests/tools/conftest.py', local('/home/developer/beremiz/tests/tools/conftest.py'))
    ERROR: could not load /home/developer/beremiz/tests/tools/conftest.py

--- tests/tools/run_python_tests.sh
+++ tests/tools/run_python_tests.sh
@@ -1,16 +1,27 @@
 #!/bin/sh
 
+cleanup()
+{
+    find ./tests/tools -name '*.pyc' -delete
+}
+
 LC_ALL=ru_RU.utf-8
 
 export DISPLAY=:42
 Xvfb $DISPLAY -screen 0 1280x1024x24 &
 sleep 1
 
+
+cleanup
+
 ret=0
 DELAY=400
 KILL_DELAY=$(($DELAY + 30))
 timeout -k $KILL_DELAY $DELAY pytest --timeout=10 ./tests/tools
 ret=$?
+
+cleanup
+
 
 pkill -9 Xvfb
 
