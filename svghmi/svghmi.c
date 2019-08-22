239533f74d4fd127f5598fa8ff0e6bcbfb117dcc
Edouard Tisserant <none@none>
2019-08-22 14:58:53 +0200

    SVGHMI: draft for svghmi.c. It has all PLC variables pointed in HMI tree in an array.
    --HG--
    branch : svghmi

--- svghmi/svghmi.c
+++ svghmi/svghmi.c
@@ -1,7 +1,37 @@
+#include "iec_types_all.h"
+#include "POUS.h"
+#include "config.h"
+#include "beremiz.h"
 
-void test_me(void){
-    /*
-    %(hmi_tree)s
-    */
-    return;
+%(extern_variables_declarations)s
+
+typedef const struct {
+    void *ptr;
+    __IEC_types_enum type;
+    /* TODO : w/r buffer, flags, locks */
+} hmi_tree_item_t;
+
+static hmi_tree_item_t hmi_tree_item[] = {
+%(variable_decl_array)s
+};
+
+int __init_svghmi()
+{
+    %(varinit)s
+    return 0;
 }
+
+void __cleanup_svghmi()
+{
+}
+
+void __retrieve_svghmi()
+{
+%(varret)s
+}
+
+void __publish_svghmi()
+{
+%(varpub)s
+}
+
