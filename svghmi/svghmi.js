b5822910785c1a56617e3400d5342cc72c15b5df
Edouard Tisserant <none@none>
2019-09-20 13:38:58 +0200

    SVGHMI: Starting to define JS side more in details.
    --HG--
    branch : svghmi

--- svghmi/svghmi.js
+++ svghmi/svghmi.js
@@ -1,11 +1,22 @@
+// svghmi.js
+
 (function(){
-    var relative_URI = window.location.href.replace(/^http(s?:\/\/[^\/]*)\/.*$/, 'ws$1/ws');
-    var ws = new WebSocket(relative_URI);
+    // Open WebSocket to relative "/ws" address
+    var ws = new WebSocket(window.location.href.replace(/^http(s?:\/\/[^\/]*)\/.*$/, 'ws$1/ws'));
+
+    // Register message reception handler 
     ws.onmessage = function (evt) {
+        // TODO : dispatch and cache hmi tree updates
+
         var received_msg = evt.data;
         alert("Message is received..."+received_msg); 
     };
+
+    // Once connection established
     ws.onopen = function (evt) {
+        // TODO : enable the HMI (was previously offline, or just starts)
+        //        show main page
+
         ws.send("test");
     };
 })();
