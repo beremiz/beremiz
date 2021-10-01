3578335af6c277b18b617abb04454cacae98abb8
Edouard Tisserant <edouard.tisserant@gmail.com>
2021-10-01 23:19:04 +0200

    Dropped SVGUI, deprecated and replaced by SVGHMI. Transformed traffic_lights exemple so that it uses SVGHMI instead.

--- features.py
+++ features.py
@@ -12,7 +12,6 @@ libraries = [
     ('Native', 'NativeLib.NativeLibrary', True),
     ('Python', 'py_ext.PythonLibrary', True),
     ('Etherlab', 'etherlab.EthercatMaster.EtherlabLibrary', False),
-    ('SVGUI', 'svgui.SVGUILibrary', False),
     ('SVGHMI', 'svghmi.SVGHMILibrary', False)]
 
 catalog = [
@@ -23,7 +22,6 @@ catalog = [
     ('c_ext', _('C extension'), _('Add C code accessing located variables synchronously'), 'c_ext.CFile'),
     ('py_ext', _('Python file'), _('Add Python code executed asynchronously'), 'py_ext.PythonFile'),
     ('wxglade_hmi', _('WxGlade GUI'), _('Add a simple WxGlade based GUI.'), 'wxglade_hmi.WxGladeHMI'),
-    ('svgui', _('SVGUI'), _('Experimental web based HMI'), 'svgui.SVGUI'),
     ('svghmi', _('SVGHMI'), _('SVG based HMI'), 'svghmi.SVGHMI')]
 
 file_editors = []
