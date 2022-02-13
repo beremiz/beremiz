b719598ee8b530bad5108614770a3a4ad396fcc0
Edouard Tisserant <edouard.tisserant@gmail.com>
2022-02-13 21:01:27 +0100

    Add Docker image definition as reference platform to run IDE tests.
    --HG--
    branch : wxPython4

---
+++ tests/tools/Docker/beremiz-sikuli/create_docker_container.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -e
+
+# source directory containing beremiz, matiec, etc..
+SRCDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd ../../../../.. && pwd )"
+echo "SOURCE direcory : $SRCDIR"
+
+# absolute path to test directory. ~/test if not given as only argument
+TESTDIR=${1:-~/test}
+mkdir -p $TESTDIR
+echo "TEST direcory : $TESTDIR"
+
+UNAME=testing
+UHOME=/home/$UNAME
+
+# define TESTDEBUG in env to enable dev-mode debug pasthrough
+# for x11vnc and Xnest
+DEBUGARGS="-v /tmp/.X11-unix/X0:/tmp/.X11-unix/X0 -e DISPLAY=$DISPLAY"
+
+# In case VNC with x11vnc is prefered, then a port should bepassed like this
+# DEBUGARGS="-p 5900:5900"
+
+echo "Creating docker container"
+docker create \
+       --name beremiz_sikuli_current \
+       -v $SRCDIR:$UHOME/src \
+       -v $TESTDIR:$UHOME/test \
+       `if [ "$TESTDEBUG" == "YES" ]; then echo $DEBUGARGS; fi` \
+       -w $UHOME/test \
+       -i -t beremiz_sikuli /bin/bash
+
