739889a33d623ea776b5f3aa5d439bcc2d76e9cc
Edouard Tisserant <edouard.tisserant@gmail.com>
2022-03-05 11:14:00 +0100

    Tests: Enhance robustness of stdout driven waiting state in Sikuli based tests.
    Some tests were randomly passing, because from time to time waiting for idle was skiped. It was combination of multiple problems :
        - buffering on stdout (now use readline + flush for each write to log)
        - it is sometime required to wait for activity before waiting for timeout added "WaitForChangeAndIdle" to "stdoutIdleObserver"
    
    --HG--
    branch : wxPython4

--- tests/ide_tests/edit_project.sikuli/edit_project.py
+++ tests/ide_tests/edit_project.sikuli/edit_project.py
@@ -15,46 +15,55 @@ from sikuliberemiz import *
 proc,app = StartBeremizApp(exemple="python")
 
 # To detect when actions did finish because IDE content isn't changing
-# idle = IDEIdleObserver(app)
-# screencap based idle detection was making many false positive. Test is more stable with stdout based idle detection
+idle = IDEIdleObserver(app)
 
 doubleClick("1646062660770.png")
 
+idle.Wait(1,15)
+
 click("1646066794902.png")
+
+idle.Wait(1,15)
 
 type(Key.DOWN * 10, Key.CTRL)
 
+idle.Wait(1,15)
+
 doubleClick("1646066996620.png")
+
+idle.Wait(1,15)
 
 type(Key.TAB*3)  # select text content
 
-type("'sys.stdout.write(\"EDIT TEST OK\")'")
+type("'sys.stdout.write(\"EDIT TEST OK\\n\")'")
 
 type(Key.ENTER)
 
-stdoutIdle = stdoutIdleObserver(proc)
+idle.Wait(1,15)
 
-# To send keyboard shortuts
 k = KBDShortcut(app)
+
+k.Save()
+
+del idle
+
+stdoutIdle = stdoutIdleObserver(proc)
 
 k.Clean()
 
-stdoutIdle.Wait(2,15)
+stdoutIdle.WaitForChangeAndIdle(2,15)
 
-k.Save()
 k.Build()
 
-stdoutIdle.Wait(2,15)
+stdoutIdle.WaitForChangeAndIdle(2,15)
 
 k.Connect()
 
-stdoutIdle.Wait(2,15)
+stdoutIdle.WaitForChangeAndIdle(2,15)
 
 k.Transfer()
 
-stdoutIdle.Wait(2,15)
-
-#del idle
+stdoutIdle.WaitForChangeAndIdle(2,15)
 
 del stdoutIdle
 
