6a90ed4e0d8d4afec2765c8902764202171e93ac
Edouard Tisserant <none@none>
2022-11-09 12:14:35 +0100

    SVGHMI: fix HMI freeze at load when no screen saver is defined

--- svghmi/svghmi.js
+++ svghmi/svghmi.js
@@ -428,18 +428,22 @@ document.body.addEventListener('contextmenu', e => {
     e.preventDefault();
 });
 
-var screensaver_timer = null;
-function reset_screensaver_timer() {
-    if(screensaver_timer){
-        window.clearTimeout(screensaver_timer);
+if(screensaver_delay){
+    var screensaver_timer = null;
+    function reset_screensaver_timer() {
+        if(screensaver_timer){
+            window.clearTimeout(screensaver_timer);
+        }
+        screensaver_timer = window.setTimeout(() => {
+            switch_page("ScreenSaver");
+            screensaver_timer = null;
+        }, screensaver_delay*1000);
     }
-    screensaver_timer = window.setTimeout(() => {
-        switch_page("ScreenSaver");
-        screensaver_timer = null;
-    }, screensaver_delay*1000);
-}
-if(screensaver_delay)
     document.body.addEventListener('pointerdown', reset_screensaver_timer);
+    // initialize screensaver
+    reset_screensaver_timer();
+}
+
 
 function detach_detachables() {
 
@@ -600,9 +604,6 @@ detach_detachables();
 
 // show main page
 switch_page(default_page);
-
-// initialize screensaver
-reset_screensaver_timer();
 
 var reconnect_delay = 0;
 var periodic_reconnect_timer;
