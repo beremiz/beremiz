94e42cda0a8e093aac4830b5c40656c240ec8cf9
Edouard Tisserant <edouard.tisserant@gmail.com>
2023-10-12 17:03:44 +0200

    IDE: fix exception when adding Action

--- plcopen/BlockInstanceCollector.py
+++ plcopen/BlockInstanceCollector.py
@@ -100,7 +100,7 @@ class BlockInstanceFactory(object):
     def __init__(self, block_instances):
         self.BlockInstances = block_instances
         self.CurrentInstance = None
-        self.SpecificValues = None
+        self.SpecificValues = []
         self.CurrentConnection = None
         self.CurrentLink = None
 
