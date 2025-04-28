1d52d756484ccda10711b61e69b913c683186f92
Edouard Tisserant <edouard.tisserant@gmail.com>
2025-04-28 14:04:24 +0200

    Zephyr: Fix non-programmable build, unconditionally starts PLC.

--- C_runtime/zephyr/main_builtin.c
+++ C_runtime/zephyr/main_builtin.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
+#include <zephyr/kernel.h>
+
+/* 1000 msec = 1 sec */
+#define SLEEP_TIME_MS   1000
+
+
+int startPLC(int argc, char **argv);
 
 int main(void)
 {
 	printf("Beremiz non-programmable runtime for Zephyr\n");
+
+	// This create a thread for the PLC and starts the PLC
+	int res = startPLC(0, NULL);
+	printf("PLC started (startPLC = %d)\n", res);
+
+	// Temporarily loop forever - TODO: answer basic tty cli instead
+	while (1) {
+        k_msleep(SLEEP_TIME_MS);
+    }
+
 	return 0;
 }
