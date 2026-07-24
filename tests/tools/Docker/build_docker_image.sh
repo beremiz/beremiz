589bdc57e1af7c26e8175715896ab4c002a78c62
Edouard Tisserant <edouard.tisserant@gmail.com>
2026-07-24 22:16:21 +0200

    tests: fix broken Dockerfile and requirements files
    Consequences of commit "IDE+Tests: update and rename requirements".
    Cached test docker image prevented detecting broken docker build and crossbar setup.

--- tests/tools/Docker/build_docker_image.sh
+++ tests/tools/Docker/build_docker_image.sh
@@ -3,7 +3,8 @@
 set -e
 
 echo "Building docker image"
-cp -f ../../../requirements.txt requirements.txt
+cp -f ../../../requirements_pinned.txt requirements_pinned.txt
+export BUILDKIT_PROGRESS=plain
 docker build \
     --build-arg UID=$(id -u) \
     --build-arg GID=$(id -g) \
